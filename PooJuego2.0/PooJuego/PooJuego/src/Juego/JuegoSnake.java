2262cc39da8f5d3665e6ce908ba877727ad8b3b6
Ariel Tandazo <166523269+YooneR1209@users.noreply.github.com>
2024-08-04 19:28:44 -0500

    Actualización UML
    UML update

--- PooJuego2.0/PooJuego/PooJuego/src/Juego/JuegoSnake.java
+++ PooJuego2.0/PooJuego/PooJuego/src/Juego/JuegoSnake.java
@@ -3,10 +3,10 @@ package Juego;
 
 import java.awt.event.KeyEvent;
 
-public class Vista extends javax.swing.JFrame {
+public class JuegoSnake extends javax.swing.JFrame {
     
     Serpiente panel;
-    public Vista() {
+    public JuegoSnake() {
         initComponents();
         this.setTitle("Juego de la Serpiente"); 
         this.setLocationRelativeTo(null);
@@ -89,20 +89,21 @@ public class Vista extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Vista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(JuegoSnake.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Vista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(JuegoSnake.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Vista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(JuegoSnake.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Vista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(JuegoSnake.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Vista().setVisible(true);
+                new JuegoSnake().setVisible(true);
             }
         });
     }
